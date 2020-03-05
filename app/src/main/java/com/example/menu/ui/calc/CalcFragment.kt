e75913c126e9fff4da3ed916fcffe2d37ac690e4
naodps <nao.yamato@dpschool.io>
2020-03-05 11:48:12 +0100

    spell edit

--- app/src/main/java/com/example/menu/ui/calc/CalcFragment.kt
+++ app/src/main/java/com/example/menu/ui/calc/CalcFragment.kt
@@ -1,4 +1,5 @@
-package com.example.menu.ui.culc
+package com.example.menu.ui.calc
+
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,21 +10,22 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.menu.R
+import com.example.menu.ui.calc.CalcViewModel
 
-class CulcFragment : Fragment() {
+class CalcFragment : Fragment() {
 
-    private lateinit var culcViewModel: CulcViewModel
+    private lateinit var calcViewModel: CalcViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        culcViewModel =
-            ViewModelProviders.of(this).get(CulcViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_culc, container, false)
-        val textView: TextView = root.findViewById(R.id.text_culc)
-        culcViewModel.text.observe(viewLifecycleOwner, Observer {
+        calcViewModel =
+            ViewModelProviders.of(this).get(CalcViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_calc, container, false)
+        val textView: TextView = root.findViewById(R.id.text_calc)
+        calcViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/example/menu/ui/calc/CalcViewModel.kt
+++ app/src/main/java/com/example/menu/ui/calc/CalcViewModel.kt
@@ -1,14 +1,13 @@
-package com.example.menu.ui.culc
-
+package com.example.menu.ui.calc
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class CulcViewModel : ViewModel() {
+class CalcViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is culc Fragment"
+        value = "This is calc Fragment"
     }
     val text: LiveData<String> = _text
 }
